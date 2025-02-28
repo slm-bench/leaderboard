b90a4f22afeb740e1a3bfbb2671d756dc5ee7055
Nghiem Thanh Pham <nghiempt@farmcode.io.vn>
2025-02-28 10:12:44 +0700

    fix: hidden scroll

--- src/modules/home/index.tsx
+++ src/modules/home/index.tsx
@@ -10,16 +10,27 @@ export default function HomeClient() {
       display: "flex",
       flexDirection: "column",
       alignItems: "center",
-      justifyContent: "center"
+      justifyContent: "center",
+      overflow: "hidden" // Prevents outer scrollbars
     }}>
       <iframe
         src={"https://slm-bench-leaderboard.vercel.app/"}
         title="embedded"
+        frameBorder="0"
         style={{
           width: "100%",
-          height: "100vh"
+          height: "100vh",
+          border: "none", // Explicitly remove border
+          scrollbarWidth: "none", // Firefox
+          msOverflowStyle: "none", // IE and Edge
         }}
       />
+      {/* CSS to hide webkit scrollbars (Chrome, Safari) */}
+      <style jsx global>{`
+        iframe::-webkit-scrollbar {
+          display: none;
+        }
+      `}</style>
     </div>
   )
 }
