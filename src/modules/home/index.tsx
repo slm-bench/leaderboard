95ae19a76fe7753f3ccafb901c85c835ad9bddc6
Nghiem Thanh Pham <nghiempt@farmcode.io.vn>
2025-02-28 09:40:05 +0700

    test: embed other web

--- src/modules/home/index.tsx
+++ src/modules/home/index.tsx
@@ -1,53 +1,15 @@
 "use client"
 
-import React from 'react';
-import { Button } from "@/components/ui/button";
-import { Crown } from 'lucide-react';
-import MedalTable from './table';
-import DataAnalysisSection from './data';
-import './styles/index.css';
+import React, { useState } from 'react';
 
 export default function HomeClient() {
   return (
-    <div className="container">
-      <div className="background">
-        <section className="content-section">
-          <div className="header-container">
-            <div className="title-container">
-              <h1 className="main-title">Small Language Models</h1>
-              <div className="crown-container">
-                <Crown className="crown-icon" />
-              </div>
-            </div>
-            <div className="subtitle-container">
-              <h2 className="subtitle-heading">All we know about</h2>
-              <h3 className="subtitle-subheading">Small LLMs</h3>
-              <div className="gradient-container">
-                <span className="gradient-text">on Edge Devices</span>
-              </div>
-            </div>
-            <p className="description">
-              Survey, Measurements, and Insights
-            </p>
-            <div className="button-container">
-              <Button variant="outline" className="action-button">
-                <span className="button-icon">📊</span>
-                Accuracy
-              </Button>
-              <Button variant="outline" className="action-button">
-                <span className="button-icon">💰</span>
-                Cost on Device
-              </Button>
-              <Button variant="outline" className="action-button">
-                <span className="button-icon">📄</span>
-                Paper
-              </Button>
-            </div>
-          </div>
-        </section>
-        <MedalTable />
-        <DataAnalysisSection />
-      </div>
+    <div className="w-full h-screen flex flex-col items-center justify-center">
+      <iframe
+        src={"https://www.ieltsviet.edu.vn/"}
+        title="embedded"
+        className="w-full h-screen"
+      />
     </div>
   )
 }
